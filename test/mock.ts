a6d358583dec8568f05611cdd7e120b8875a1b37
mhchia <kevin.mh.chia@gmail.com>
2020-06-01 02:07:41 +0800

    Add a mock test for `SMPPeer`
    `Peer` is entirely mocked since it doesn't work with nodejs

---
+++ test/mock.ts
@@ -0,0 +1,91 @@
+import { randomBytes } from 'crypto';
+
+type CBDataConnectionOpen = () => Promise<void>;
+type CBDataConnectionData = (data: ArrayBuffer) => void;
+
+class MockDataConnection {
+  peer: string;
+  openCB?: CBDataConnectionOpen;
+  dataCB?: CBDataConnectionData;
+  remoteConn?: MockDataConnection;
+  constructor(peerID: string) {
+    this.peer = peerID;
+  }
+
+  on(event: string, cb: CBDataConnectionOpen | CBDataConnectionData) {
+    if (event === 'open') {
+      // TODO: Add type checking here.
+      this.openCB = cb as CBDataConnectionOpen;
+      // Handle both sync and async.
+      this.openCB();
+    } else if (event === 'data') {
+      this.dataCB = cb as CBDataConnectionData;
+    } else {
+      throw Error(`event ${event} is not supported`);
+    }
+  }
+
+  setRemote(remoteConn: MockDataConnection) {
+    this.remoteConn = remoteConn;
+  }
+
+  send(data: ArrayBuffer) {
+    if (this.remoteConn === undefined || this.remoteConn.dataCB === undefined) {
+      throw new Error();
+    }
+    this.remoteConn.dataCB(data);
+  }
+}
+
+type CBPeerConnection = (conn: MockDataConnection) => void;
+type CBPeerOpen = (id: string) => void;
+
+const peers = new Map<string, MockPeer>();
+
+export class MockPeer {
+  id: string;
+  connectionCB?: CBPeerConnection;
+  conns: Map<string, MockDataConnection>;
+
+  constructor(id?: string, options?: any) {
+    if (id === undefined) {
+      this.id = randomBytes(32).toString('hex');
+    } else {
+      this.id = id;
+    }
+    peers.set(this.id, this);
+    console.log("MOCKED!");
+    this.conns = new Map();
+  }
+
+  on(event: string, cb: CBPeerOpen | CBPeerConnection): void {
+    if (event === 'open') {
+      (cb as CBPeerOpen)(this.id);
+    } else if (event === 'connection') {
+      this.connectionCB = cb as CBPeerConnection;
+    } else {
+      throw Error(`event ${event} is not supported`);
+    }
+  }
+
+  connect(remotePeerID: string, options?: any) {
+    const remotePeer = peers.get(remotePeerID);
+    if (remotePeer === undefined) {
+      throw new Error(`remotePeer=${remotePeer} is not discovered`)
+    }
+    if (this.connectionCB === undefined || remotePeer.connectionCB === undefined) {
+      throw new Error("either `localPeer` or `remotePeer` hasn't called `on`");
+    }
+    const localConn = new MockDataConnection(this.id);
+    const remoteConn = new MockDataConnection(remotePeerID);
+    localConn.setRemote(remoteConn);
+    remoteConn.setRemote(localConn);
+    this.conns.set(remotePeerID, remoteConn);
+    remotePeer.conns.set(this.id, localConn);
+
+    // The callback of remote is called.
+    remotePeer.connectionCB(remoteConn);
+
+    return localConn;
+  }
+}
--- test/smpPeer.test.ts
+++ test/smpPeer.test.ts
@@ -1,15 +1,43 @@
-// import SMPPeer from '../src/smpPeer';
+/**
+ * NOTE: Mock `Peer` with `MockPeer` since peerjs doesn't work w/ nodejs.
+ * Ref:
+ *  - https://github.com/mhchia/peek-a-book/issues/2
+ *  - https://github.com/peers/peerjs/issues/606
+ * Error: "node_modules/peerjs/dist/peerjs.min.js:48 ERROR PeerJS:  Error: The current browser does not support WebRTC"
+ */
 
-describe('', () => {
-  test('test', async () => {
-    // TODO: peerjs doesn't work w/ nodejs. Figure out a way to run tests.
-    //  Ref:
-    //    - https://github.com/mhchia/peek-a-book/issues/2
-    //    - https://github.com/peers/peerjs/issues/606
-    //  Error: "node_modules/peerjs/dist/peerjs.min.js:48 ERROR PeerJS:  Error: The current browser does not support WebRTC"
+import { MockPeer } from './mock';
+import SMPPeer from '../src/smpPeer';
 
-    // const pid = '123456';
-    // const t = new SMPPeer('1');
-    // await t.connectToPeerServer(pid);
+jest.mock('peerjs', () => {
+  return MockPeer;
+});
+
+describe('SMPPeer', () => {
+  const params = [
+    ['1', '1'],
+    ['1', '2'],
+  ]
+  const expectedResults = [
+    true,
+    false,
+  ];
+  test('Succeeds', async () => {
+    for (const i in params) {
+      const param = params[i];
+      const expectedResult = expectedResults[i];
+      const actual = await smp(param[0], param[1]);
+      expect(actual).toEqual(expectedResult);
+    }
   });
 });
+
+async function smp(x: string, y: string) {
+  const peerIDA = 'A';
+  const peerIDB = 'B';
+  const alice = new SMPPeer(x, peerIDA);
+  const bob = new SMPPeer(y, peerIDB);
+  await alice.connectToPeerServer();
+  await bob.connectToPeerServer();
+  return (await alice.runSMP(peerIDB));
+}
